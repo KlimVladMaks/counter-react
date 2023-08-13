c6f45b15d2402165d559937bb4d0d41d11d54be8
KlimVladMaks <vkm.blog@yandex.ru>
2023-08-13 22:41:43 +0300

    Реализован механизм работы счётчика

--- src/App.js
+++ src/App.js
@@ -6,13 +6,58 @@ import React from 'react';
  * @returns {JSX.Element} Разметка для отображения счётчика.
  */
 function App() {
+
+  const [count, setCount] = React.useState(0);
+
+  /**
+   * Функция для увеличения счётчика на единицу.
+   */
+  const onClickPlus = () => {
+    setCount(count + 1);
+  }
+
+  /**
+   * Функция для уменьшения счётчика на единицу.
+   */
+  const onClickMinus = () => {
+    setCount(count - 1);
+  }
+
+  /**
+   * Функция для умножения счётчика на два.
+   */
+  const onClickMultiply = () => {
+    setCount(count * 2);
+  }
+
+  /**
+   * Функция для деления счётчика на два.
+   */
+  const onClickDivide = () => {
+    setCount(Math.round(count / 2));
+  }
+
+  /**
+   * Функция для возведения счётчика во вторую степень.
+   */
+  const onClickSquare = () => {
+    setCount(count ** 2);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счётчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <div className='button-container'>
+          <button className="red" onClick={onClickMinus}>- Минус</button>
+          <button className="green" onClick={onClickPlus}>Плюс +</button>
+        </div>
+        <div className='button-container'>
+          <button className='red' onClick={onClickDivide}>/ Разделить</button>
+          <button className='green' onClick={onClickMultiply}>Умножить *</button>
+        </div>
+        <button className='yellow' onClick={onClickSquare}>Возвести в степень ^</button>
       </div>
     </div>
   );
